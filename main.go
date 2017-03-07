dd4a80f0acdcd4c89e5fca09d377e7afd08da1c8
Andrew Watson <andrewmoorewatson@gmail.com>
2017-03-07 21:43:14 +0000

    detect long and short vibration periods

--- main.go
+++ main.go
@@ -21,8 +21,9 @@ const (
 
 	// how many cycles do we go through with the same state to determine
 	// that the sensor is no longer vibrating
-	stateChangeThreshold = 50
-	cycleDelay           = 20 * time.Millisecond
+	stateChangeThreshold = 20
+	cycleDelay           = 5 * time.Millisecond
+	minimumDuration      = 2000 // in Millisecond
 )
 
 func main() {
@@ -39,33 +40,50 @@ func main() {
 	sensor := rpio.Pin(pinSense)
 	sensor.Input()
 
-	currentState = int(sensor.Read())
+	lastReading = sensor.Read()
 
+	var lastStateChange time.Time
 	unchangedState := 0
 
 	for {
 
+		rightNow := time.Now()
 		currentReading = sensor.Read()
 
-		if lastReading == currentReading {
+		if lastReading != currentReading {
+
+			// the pin state has changed!
+			unchangedState = 0
+			if currentState == stateStill {
+				currentState = stateMoving
+				lastStateChange = rightNow
+				// fmt.Printf("Started Moving: %s\n", rightNow.Format(time.Stamp))
+			}
+
+		} else {
+
 			unchangedState++
 
-			if unchangedState > stateChangeThreshold && currentState != stateStill {
-				currentState = stateStill
-				fmt.Println("State Changed to Still")
+			if unchangedState > stateChangeThreshold {
+				// we've been in the same same condition long enough to assume we're not moving
+
+				if currentState == stateMoving {
+					// we were moving but we've stopped now
+					currentState = stateStill
+
+					duration := rightNow.Sub(lastStateChange).Nanoseconds() / 1000000
+
+					fmt.Printf("Vibration Ended After %d ms\n", duration)
+					lastStateChange = rightNow
+
+					if duration > minimumDuration {
+						fmt.Println("LONG VIBRATION DETECTED")
+					}
+
+				}
+
 				unchangedState = 0
-
 			}
-		}
-
-		if lastReading != currentReading {
-			// fmt.Printf("Sensor Change: %#v\n", currentReading)
-
-			if currentState == stateStill && currentReading == rpio.High {
-				currentState = stateMoving
-				fmt.Println("Vibration Started")
-			}
-
 		}
 
 		lastReading = currentReading
